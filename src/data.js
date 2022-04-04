1187f68460466807ba06c7b2b8e3242bb08b96df
shubham_11 <shubhamganeshsonawane11@gmail.com>
2022-04-04 15:36:14 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      id: 1,
+      name: 'shubham sonawane',
+      age: 21,
+      image:
+        '',
+    },
+    {
+      id: 2,
+      name: 'mahesh patil',
+      age: 23,
+      image:
+        '',
+    },
+    {
+      id: 3,
+      name: 'king kohali',
+      age: 21,
+      image:
+        ' ',
+    },
+    {
+      id: 4,
+      name: 'neha desale',
+      age: 20,
+      image:
+        ' ',
+    },
+    {
+      id: 5,
+      name: 'perana deore',
+      age: 21,
+      image:
+        ' ',
+    },
+  ];
